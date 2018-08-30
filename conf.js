7faa79d168fec5f9bebcaeb291b6d8e6d57decb3
Zhou houfa <houfa.zhou@thingtrust.com>
2018-08-30 15:41:02 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -8,7 +8,11 @@ exports.bLight = false;
 
 exports.storage = 'sqlite';
 
+<<<<<<< HEAD
 exports.hub = '127.0.0.1:6616';
+=======
+exports.hub = 'test.mainchain.pow.trustnote.org:9191';
+>>>>>>> Update conf.js
 exports.deviceName = 'Headless';
 exports.permanent_pairing_secret = 'randomstring';
 exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
