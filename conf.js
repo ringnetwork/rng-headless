cfb46b35f58ceef2d9b46e020c93e3194e27ee89
ianhhhhhhhhe <517382432@qq.com>
2018-10-10 17:07:49 +0800

    Update: add initial witness list

--- conf.js
+++ conf.js
@@ -8,12 +8,24 @@ exports.bLight = false;
 
 exports.storage = 'sqlite';
 
-exports.hub = 'victor.trustnote.org/tn';
+exports.hub = '127.0.0.1:6616';
 exports.deviceName = 'Headless';
 exports.permanent_pairing_secret = 'randomstring';
 exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
 exports.payout_address = 'WHERE THE MONEY CAN BE SENT TO';
 exports.KEYS_FILENAME = 'keys.json';
+
+exports.initialWitnesses = [
+    '72FZXZMFPESCMUHUPWTZJ2F57YV32JCI',
+    '2G6WV4QQVF75EPKSXTVRKRTZYSXNIWLU',
+    '4ZJ3HQTLYK72O4PLE3GA4ZYCYXLIFHXK',
+    '7RR5E6BRHE55FHE76HO6RT2E4ZP3CHYA',
+    'CAGSFKGJDODHWFJF5LS7577TKVPLH7KG',
+    'FX2B6E622RF4J4MM2OUWMGSOKJP7XTXB',
+    'JN2N7SOMDKNSDGMVAW346BYTOSKZIIT4',
+    'SAHCPBJAAOXRJ6KRSM3OGATIRSWIWOQA',
+    'WL44BDM4QNCMAM5AS3ZB2GYTVDBWAS5Z'
+];
 
 // where logs are written to (absolute path).  Default is log.txt in app data directory
 //exports.LOG_FILENAME = '/dev/null';
