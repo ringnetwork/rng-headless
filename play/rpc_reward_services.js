af50d3faf970788b929bf17139c034bcbf4776e4
VictorZhang <victor55511@163.com>
2019-06-10 18:15:34 +0800

    auto reward

--- play/rpc_reward_services.js
+++ play/rpc_reward_services.js
@@ -316,8 +316,7 @@ function initRPC() {
 							if(err)
 								onError(err);
 							createRewardPayment(rewardPeriod, totalReward, function(err){
-								console.log("AutoRewardPeriod Manual finished");
-								//console.log("AutoRewardPeriod Manual finished:" + rewardPeriod + "," + err ? err : "succeed!");
+								console.log("AutoRewardPeriod Manual finished:" + rewardPeriod + "," + (err ? err : "succeed!"));
 								cb( err ? err : "succeed!" );
 							});
 						});	
